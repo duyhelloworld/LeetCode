df8792c18e5fca4664c32d5e763dcf63272d4e98
duyhelloworld <codedaovoiduy@gmail.com>
2023-02-11 20:21:26 +0700

    modified:   Guide/Problem1342.java

--- Guide/Problem1342.java
+++ Guide/Problem1342.java
@@ -5,15 +5,19 @@ import java.util.Scanner;
 public class Problem1342 {
     public static void main(String[] args) {
         try (Scanner scanner = new Scanner(System.in)) {
-            System.out.println("Enter number n = ");
+            System.out.print("Enter number n = ");
             int n = scanner.nextInt(), count = 0;
 
-            do {
-                if (n % 2 == 0) {
-                    
-                }
-            } while (n % 2 != 0);
-
+            while (n != 0) {
+                count++;
+                n = n / 2;
+            }
+                // if (n % 2 == 0) {
+                    // count++;
+                    // n = n % 2;
+                // } else {
+                // }
+            System.out.println(count);
         } catch (Exception e) {
             e.printStackTrace();
         }
