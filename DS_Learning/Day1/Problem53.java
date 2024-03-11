5f5b1a6805d71b1d1cc15866c0a3f30e68abb877
duyhelloworld <codedaovoiduy@gmail.com>
2024-03-11 20:02:33 +0700

    Hoc Problem53

--- DS_Learning/Day1/Problem53.java
+++ DS_Learning/Day1/Problem53.java
@@ -1,66 +1,64 @@
 package Day1;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-
 /**
  * Given an integer array nums, find the
-subarray
+    subarray
 with the largest sum, and return its sum.
  */
 public class Problem53 {
     public static int getLargest(int[] arr){
-        int negative = 0;
-        for (int i = 0; i < arr.length; i++) {
-            if (arr[i] < 0) {
-                negative++;
-            }
-        }
+        // Count frequency negative number
+        // int negative = 0;
+        // int length = arr.length;
+        // for (int i = 0; i < length; i++) {
+        //     if (arr[i] < 0) {
+        //         negative++;
+        //     }
+        // }
         
-        int MaxSum = 0;
+        // int MaxSum = 0;
+        // // If array is fully positive, return itself
+        // if (negative == 0) {
+        //     for (int element : arr) {
+        //         MaxSum += element;
+        //     }
+        //     return MaxSum;
+        // }
 
-        if (negative == 0) {
-            for (int element : arr) {
-                MaxSum += element;
-            }
-            return MaxSum;
-        }
-
-        if (negative == arr.length) {
-            Arrays.sort(arr);
-            return arr[arr.length - 1];
-        }
+        // if (negative == length) {
+        //     Arrays.sort(arr);
+        //     return arr[length - 1];
+        // }
         
-        List<Integer> sums = new ArrayList<>();
+        // // Count if array is mix of positive and negative
+        // List<Integer> sums = new ArrayList<>();
 
-        for (int i = 0; i < arr.length; i++) {
-            if (arr[i] > 0) {
-                int j = i;
-                int summary = 0;
-                while(j < arr.length) {
-                    summary += arr[j];
-                    sums.add(summary);
-                    j++;
-                }
-            }
-        }
+        // for (int i = 0; i < length; i++) {
+        //     if (arr[i] > 0) {
+        //         int j = i;
+        //         int summary = 0;
+        //         while(j < length) {
+        //             summary += arr[j];
+        //             sums.add(summary);
+        //             j++;
+        //         }
+        //     }
+        // }
 
-        if (sums.isEmpty()) {
-            return 0;
-        }
+        // // No any subarray
+        // if (sums.isEmpty()) {
+        //     return 0;
+        // }
 
-        int max = sums.get(0);
-        for (int _sum : sums) {
-            if (_sum > max) {
-                max = _sum; 
-            }
-        }
-        return max;
+        // int max = sums.get(0);
+        // for (int _sum : sums) {
+        //     if (_sum > max) {
+        //         max = _sum; 
+        //     }
+        // }
+        // return max;
 
-
-
-        // Copy logic
+        // Optimized code
         // int sum = 0, max = arr[0];
         // for (int i = 0; i < arr.length; i++) {
         //     sum += arr[i];
@@ -74,25 +72,24 @@ public class Problem53 {
         // return max;
 
         // Other implementation
-        // int sum = 0 , maxi = Integer.MIN_VALUE;;
-        // int i = 0 , j = 0;
-        // while(j < arr.length){
-           
-        //     if(sum < 0 && arr[j] >= sum){
-        //         sum = 0;
-        //         i = j;
-        //     }
-        //      sum += arr[j];
-        //     maxi = Math.max(maxi , sum);
-        //     j++;
-        // }
-        // return maxi;
+        int sum = 0, max = Integer.MIN_VALUE;
+        int i = 0;
+        while(i < arr.length){
+            if (sum < 0 && arr[i] >= sum) {
+                sum = 0;
+            }
+            sum += arr[i];
+            max = Math.max(max, sum);
+            i++;
+        }
+        return max;
     }
 
     public static void main(String[] args) {
-        // int[] arr = {-2, 1, -3, 4, 9, -8, 0};
-        // int[] arr = {-2,  1,  -3,  4,  -1,  2,  1,  -5,  4};
-        int[] arr = {-1, 2};
-        System.out.println(getLargest(arr));
+        // int[] arr = {-2, 1, -3, 4, 9, -8, 0};                    // 13
+        // int[] arr = { -2,  1,  -3,  4,  -1,  2,  1,  -5,  4};    // 6
+        // int[] arr = { -1, -1, -10, -2, -2 };                     // -1
+        // int[] arr = {-1, 2};                                        // 2
+        // System.out.println(getLargest(arr));
     }
 }
